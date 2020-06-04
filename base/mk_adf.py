3eede4935156df82c12aa6c6debc8b5506ab3271
Keir Fraser <keir.xen@gmail.com>
2020-06-04 07:15:20 +0100

    bootblock: Cleaner structure. Better error handling.

--- base/mk_adf.py
+++ base/mk_adf.py
@@ -24,7 +24,7 @@ def main(argv):
     with open(argv[2], "rb") as f:
         pl_dat = f.read()
 
-    bb_len = len(bb_dat) + 4
+    bb_len = len(bb_dat)
     assert (bb_len & 3) == 0
     
     _, decompressed_length, _, leeway = struct.unpack(">2I2H", pl_dat[:12])
@@ -40,8 +40,7 @@ def main(argv):
     # Amount of memory to allocate in the loader for unpacked payload.
     alloc_len = max(load_len, decompressed_length) + leeway
     alloc_len += -alloc_len & 3
-    
-    bb_dat += struct.pack(">2I", alloc_len-load_len, load_len)
+
     print(" BB + Compressed = %u + %u = %u"
           % (bb_len, pl_len, bb_len + pl_len))
     print(" Load - Compressed = %u - %u = %u"
@@ -50,11 +49,12 @@ def main(argv):
           % (alloc_len, decompressed_length, alloc_len - decompressed_length))
     print(" == %u Tracks ==" % ((load_len + 5631) // 5632))
     
-    # Splice the computed checksum into the header
-    sum = checksum(bb_dat[:1024]) ^ 0xFFFFFFFF
-    bb_dat = bb_dat[:4] + struct.pack(">L", sum) + bb_dat[8:]
+    # Concatenate everything and splice in the checksum.
+    splice = struct.pack(">2I", alloc_len-load_len, load_len)
+    image = bb_dat[0:14] + splice + bb_dat[22:] + pl_dat
+    sum = checksum(image[:1024]) ^ 0xFFFFFFFF
+    image = image[:4] + struct.pack(">L", sum) + image[8:]
 
-    image = bb_dat + pl_dat
     image += bytes(901120 - len(image))
     with open(argv[3], "wb") as f:
         f.write(image)
