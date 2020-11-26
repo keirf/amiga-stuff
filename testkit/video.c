c3815b48154b4c38eebe54d10e930af062b5b341
Keir Fraser <keir.xen@gmail.com>
2020-11-26 10:50:24 +0000

    testkit: Add video testcards for scaler calibration

--- testkit/video.c
+++ testkit/video.c
@@ -11,7 +11,7 @@
 
 #include "testkit.h"
 
-void videocheck(void)
+static void gradients(void)
 {
     uint16_t *p, *cop, wait;
     unsigned int i, j, k;
@@ -122,8 +122,7 @@ void videocheck(void)
     *p++ = 0x0180; *p++ = 0x0000;
 
     /* End of copper list. */
-    *p++ = 0xffff;
-    *p++ = 0xfffe;
+    *p++ = 0xffff; *p++ = 0xfffe;
 
     copperlist_set(cop);
 
@@ -136,4 +135,113 @@ void videocheck(void)
     /* All work is done by the copper. Just wait for exit. */
     while (!do_exit && (keycode_buffer != K_ESC))
         continue;
+}
+
+static void checkerboard(bool_t alternating)
+{
+    uint32_t *b, pattern;
+    uint16_t *p, *cop;
+    unsigned int i, j;
+
+    /* This test shows a per-pixel black-and-white checkerboard for
+     * calibrating video scalers such as OSSC. */
+
+    cop = p = allocmem(16384 /* plenty */);
+
+    /* Black background, B&W checker pattern (colours 02 & 03). */
+    *p++ = 0x0180; *p++ = 0x0000;
+    *p++ = 0x0184; *p++ = 0x0000;
+    *p++ = 0x0186; *p++ = 0x0fff;
+
+    /* Colours back to normal for navigation text. */
+    *p++ = 0xe407; *p++ = 0xfffe;
+    *p++ = 0x0184; *p++ = 0x0ddd;
+    *p++ = 0x0186; *p++ = 0x0ddd;
+
+    /* Plane 2 is filled. Plane 1 alternates (checker pattern). */
+    fill_rect(0, 0, xres, yres-11, 2);
+    b = (uint32_t *)bpl[0];
+    pattern = 0xaaaaaaaa;
+    for (i = 0; i < yres-11; i++) {
+        pattern = ~pattern;
+        for (j = 0; j < xres/32; j++)
+            *b++ = pattern;
+    }
+
+    /* End of copper list. */
+    *p++ = 0xffff; *p++ = 0xfffe;
+
+    copperlist_set(cop);
+
+    /* Wait for exit; alternate checker pattern each frame (if requested). */
+    pattern = 0;
+    while (!do_exit && (keycode_buffer != K_ESC)) {
+        if (!alternating)
+            continue;
+        wait_bos();
+        cop[3] = pattern;
+        cop[5] = pattern = ~pattern;
+    }
+}
+
+void videocheck(void)
+{
+    char s[80];
+    struct char_row r = { .s = s };
+    void *alloc_s;
+    uint8_t key = 0;
+
+    while (!do_exit) {
+
+        print_menu_nav_line();
+
+        r.x = 16;
+        r.y = 0;
+        sprintf(s, "-- Video --");
+        print_line(&r);
+
+        r.x = 3;
+        r.y = 3;
+        sprintf(s, "$1 RGB gradients & PAL/NTSC extents$");
+        print_line(&r);
+        r.y++;
+        sprintf(s, "$2 Pixel checkerboard (static)$");
+        print_line(&r);
+        r.y++;
+        sprintf(s, "$3 Pixel checkerboard (alternating)$");
+        print_line(&r);
+
+        r.y += 2;
+
+        do {
+            while (!do_exit && !(key = keycode_buffer))
+                continue;
+            keycode_buffer = 0;
+            if (key == K_ESC)
+                do_exit = 1;
+            key -= K_F1;
+        } while (!do_exit && (key >= 3));
+
+        if (do_exit)
+            break;
+
+        alloc_s = start_allocheap_arena();
+        clear_text_rows(0, 13);
+        switch (key) {
+        case 0:
+            gradients();
+            break;
+        case 1:
+            checkerboard(FALSE);
+            break;
+        case 2:
+            checkerboard(TRUE);
+            break;
+        }
+        clear_whole_screen();
+        keycode_buffer = 0;
+        copperlist_default();
+        end_allocheap_arena(alloc_s);
+
+    }
 }
