a443a4f230748172f1969800104db9c62699ab91
Keir Fraser <keir.xen@gmail.com>
2022-07-17 19:57:35 +0100

    mk_adf: Clean up filesystem bitmap updates
    Also allocate last two tracks as these are modified by Amiga Test Kit

--- base/mk_adf.py
+++ base/mk_adf.py
@@ -9,6 +9,33 @@
 
 import struct, sys
 
+def alloc_in_bitmap(image, start, nr):
+    # Bootblock is reserved and excluded from bitmap
+    start -= 2
+    if start < 0:
+        nr += start
+        start = 0
+    # Bitmap block index is 79th longword in the root block
+    off = 880*512 + 79*4
+    bm_block, = struct.unpack(">L", image[off:off+4])
+    # Update the bitmap one longword at a time
+    while nr > 0:
+        # Create bitmask of blocks we're allocating in this longword
+        x = ((1 << (start & 31)) - 1) ^ 0xFFFFFFFF
+        x &= (1 << min(nr + (start & 31), 32)) - 1
+        # Extract the current value of this longword from the bitmap
+        off = bm_block * 512 + 4 + (start//32)*4
+        y = struct.unpack(">L", image[off:off+4])[0]
+        # Assert that bits we're allocating are currently free
+        assert x&y == x
+        # Write back the modified bitmap longword
+        image[off:off+4] = struct.pack(">L", (x^0xFFFFFFFF)&y)
+        nr -= 32 - (start & 31)
+        start += 32 - (start & 31)
+    # Now recalculate the bitmap block's checksum
+    off = bm_block * 512
+    image[off:off+4] = struct.pack(">L", block_checksum(image[off+4:off+512]))
+
 def block_checksum(block, sum=0):
     xs = struct.unpack(">%dL" % (len(block)//4), block)
     for x in xs:
@@ -67,18 +94,11 @@ def main(argv):
     image[:len(dat)] = dat
 
     # Allocate our payload blocks in the filesystem bitmap.
-    off = 880*512 + 79*4 # bitmap block idx is 79th longword in root block
-    bm_block, = struct.unpack(">L", image[off:off+4])
-    off = bm_block * 512 + 4 # skip first longword (checksum)
-    nr_blocks -= 2 # bootblock is reserved and excluded from bitmap
-    while nr_blocks != 0:
-        n = min(nr_blocks, 32)
-        image[off:off+4] = struct.pack(">L", ((1<<n)-1) ^ 0xFFFFFFFF)
-        nr_blocks -= n
-        off += 4
-    # Now recalculate the bitmap block's checksum
-    off = bm_block * 512
-    image[off:off+4] = struct.pack(">L", block_checksum(image[off+4:off+512]))
+    alloc_in_bitmap(image, 0, nr_blocks)
+
+    # AmigaTestKit writes to the last two tracks in the floppy write test.
+    # Allocate those tracks in the filesystem bitmap.
+    alloc_in_bitmap(image, 158*11, 2*11)
 
     with open(argv[3], "wb") as f:
         f.write(image)
