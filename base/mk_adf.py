2f0fcd11951f07fe37a6068572f074b4855985b3
Keir Fraser <keir.xen@gmail.com>
2022-07-17 15:06:30 +0100

    Produce Workbench-mountable ADF images

--- base/mk_adf.py
+++ base/mk_adf.py
@@ -9,20 +9,28 @@
 
 import struct, sys
 
+def block_checksum(block, sum=0):
+    xs = struct.unpack(">%dL" % (len(block)//4), block)
+    for x in xs:
+        sum += x
+    return -sum & 0xffffffff
+
 # Amiga bootblock checksum
-def checksum(bb, sum=0):
-    while len(bb):
-        x, bb = struct.unpack(">L",bb[:4]), bb[4:]
-        sum += x[0]
+def bootblock_checksum(bb, sum=0):
+    xs = struct.unpack(">256L", bb[:1024])
+    for x in xs:
+        sum += x
         if sum >= (1<<32):
             sum -= (1<<32)-1
     return sum
 
 def main(argv):
     with open(argv[1], "rb") as f:
-        bb_dat = f.read()
+        bb_dat = bytearray(f.read())
     with open(argv[2], "rb") as f:
         pl_dat = f.read()
+    with open(argv[3], "rb") as f:
+        image = bytearray(f.read())
 
     bb_len = len(bb_dat)
     assert (bb_len & 3) == 0
@@ -50,12 +58,28 @@ def main(argv):
     print(" == %u Tracks ==" % ((load_len + 5631) // 5632))
     
     # Concatenate everything and splice in the checksum.
-    splice = struct.pack(">2I", alloc_len-load_len, load_len)
-    image = bb_dat[0:14] + splice + bb_dat[22:] + pl_dat
-    sum = checksum(image[:1024]) ^ 0xFFFFFFFF
-    image = image[:4] + struct.pack(">L", sum) + image[8:]
+    bb_dat[14:22] = struct.pack(">2I", alloc_len-load_len, load_len)
+    dat = bytearray(bb_dat + pl_dat)
+    dat[4:8] = struct.pack(">L", bootblock_checksum(dat) ^ 0xFFFFFFFF)
 
-    image += bytes(901120 - len(image))
+    # Splice the concatenated payload into the disk image.
+    nr_blocks = (len(dat) + 511) // 512
+    image[:len(dat)] = dat
+
+    # Allocate our payload blocks in the filesystem bitmap.
+    off = 880*512 + 79*4 # bitmap block idx is 79th longword in root block
+    bm_block, = struct.unpack(">L", image[off:off+4])
+    off = bm_block * 512 + 4 # skip first longword (checksum)
+    nr_blocks -= 2 # bootblock is reserved and excluded from bitmap
+    while nr_blocks != 0:
+        n = min(nr_blocks, 32)
+        image[off:off+4] = struct.pack(">L", ((1<<n)-1) ^ 0xFFFFFFFF)
+        nr_blocks -= n
+        off += 4
+    # Now recalculate the bitmap block's checksum
+    off = bm_block * 512
+    image[off:off+4] = struct.pack(">L", block_checksum(image[off+4:off+512]))
+
     with open(argv[3], "wb") as f:
         f.write(image)
 
